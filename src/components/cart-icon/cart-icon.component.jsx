dd8ad72c703208c61a3a9d6fd2301d0a5b692f1f
Lily Tan <lilyt93@gmail.com>
2020-11-06 17:08:00 -0500

    converted cart icon redux to context

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,22 +9,25 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
+import CartContext from '../../context/contexts/cart/cart.contextt'
+
+const CartIcon = ({ itemCount }) => {
+  const { toggleHidden } = useContext(cartContext)
+  return(
+  <div className='cart-icon' onClick={toggleHidden}>
     <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>{itemCount}</span>
   </div>
-);
+);}
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-});
+// const mapDispatchToProps = dispatch => ({
+//   toggleCartHidden: () => dispatch(toggleCartHidden())
+// });
 
 const mapStateToProps = createStructuredSelector({
   itemCount: selectCartItemsCount
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -12,11 +12,14 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CurrentUserContext from '../../context/contexts/current-user/current-user.context'
+import CartContext from '../../context/contexts/cart/cart.context'
 
 import './header.styles.scss';
 
-const Header = ({ hidden }) => {
+const Header = () => {
   const currentUser = useContext(CurrentUserContext)
+  const [hidden, setHidden] = useState(true)
+  const toggleHidden = () => setHidden(!hidden)
   return(
   <div className='header'>
     <Link className='logo-container' to='/'>
@@ -38,7 +41,12 @@ const Header = ({ hidden }) => {
           SIGN IN
         </Link>
       )}
-      <CartIcon />
+      <CartContext.Provider value = {{
+        hidden, 
+        toggleHidden
+      }}>
+        <CartIcon />
+      </CartContext.Provider>
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
