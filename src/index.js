f7a3381acc7d1a346257602af48f3f15d9ba2703
Lily Tan <lilyt93@gmail.com>
2020-11-11 18:22:42 -0500

    took redux code out of index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
@@ -12,14 +12,12 @@ import App from './App';
 import CartProvider from './providers/cart/cart.provider'
 
 ReactDOM.render(
-  <Provider store={store}>
     <CartProvider>
       <BrowserRouter>
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
       </BrowserRouter>
-    </CartProvider>
-  </Provider>,
+    </CartProvider>,
   document.getElementById('root')
 );
